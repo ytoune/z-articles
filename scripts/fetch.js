e0322aee97a294f0feb821c7097c0ed405be6964
toune <ytoune@users.noreply.github.com>
2020-11-06 00:02:36 +0900

    add

---
+++ scripts/fetch.js
@@ -0,0 +1,16 @@
+const { fetch } = require('cross-fetch')
+const { promises: fs } = require('fs')
+const path = require('path')
+
+;(async () => {
+	const list = []
+	for (let page = 1; ; ++page) {
+		const json = await fetch('https://qiita.com/api/v2/users/rithmety/items?page=' + page)
+			.then(r => r.json())
+		if (!(json && json.length)) break
+		json.map(s => list.push(s))
+	}
+	await fs.writeFile(path.resolve(__dirname, 'pages.js'), 'module.exports = ' + JSON.stringify({ list }))
+})().catch(x => {
+	console.error(x)
+})
---
+++ scripts/marked.js
@@ -0,0 +1,28 @@
+const { promises: fs } = require('fs')
+const path = require('path')
+const { list } = require('./pages')
+
+;(async () => {
+	for (const page of list) {
+		const m = page.created_at.match(/^(\d+)-(\d+)-(\d+)T/)
+		if (!m) throw new Error('?created_at')
+		const ts = m.slice(1).join('')
+		const body = page.body
+		const title = page.title
+		const tags = page.tags.map(t => t.name.toLowerCase())
+		const text = `---
+title: ${JSON.stringify(title)}
+emoji: "🔎"
+type: "idea"
+topics: ${JSON.stringify(tags)}
+published: false
+---
+
+${body}`
+		await fs.writeFile(path.resolve(__dirname, '../articles/' + ts + '__-' + page.id + '.md'), text)
+	}
+
+	// await fs.writeFile(path.resolve(__dirname, 'pages.json'), JSON.stringify(list))
+})().catch(x => {
+	console.error(x)
+})
